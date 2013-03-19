fcd22b0b874cb923930f9a13b295f38ae19e7e9d
petryjc <petryj@rose-hulman.edu>
2013-03-19 00:11:18 -0400

    Got some deck stuff working

--- Ascension/src/Deck.java
+++ Ascension/src/Deck.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class Deck {
+	ArrayList<Card> _notPlayed;
+	ArrayList<Card> _hand;
+	ArrayList<Card> _discard;
 
+	public Deck() {
+		this(new ArrayList<Card>(), new ArrayList<Card>(), new ArrayList<Card>());
+	}
+
+	public Deck(ArrayList<Card> notPlayed) {
+		this(notPlayed, new ArrayList<Card>(), new ArrayList<Card>());
+	}
+	
+	public Deck(ArrayList<Card> notPlayed, ArrayList<Card> hand, ArrayList<Card> discard) {
+		_notPlayed = notPlayed;
+		_hand = hand;
+		_discard = discard;
+	}
+
+	public boolean DrawCard() {
+		//if the not played deck is gone, replace with discard deck
+		if(_notPlayed.size() == 0) {
+			//if discard deck is also gone, you cannot draw anymore
+			if(_discard.size() == 0) {
+				return false;
+			}
+			_notPlayed.addAll(_discard);
+			_discard.clear();
+		}
+		//generate a random index and draw that card.  replaces shuffling
+		Random generator = new Random();
+		int i = generator.nextInt(_notPlayed.size());
+		//remove from not played and add to hand
+		Card c = _notPlayed.remove(i);
+		_hand.add(c);
+		return true;
+	}
+	
+	public void AddCardToDiscard(Card c) {
+		_discard.add(c);
+	}
+	
 }
--- Ascension/src/DeckTest.java
+++ Ascension/src/DeckTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.junit.Test;
 
 
@@ -9,6 +12,83 @@ public class DeckTest {
 	public void testInitialization() {
 		Deck d = new Deck();
 		assertNotNull(d);
+	}
+	
+	@Test
+	public void testDeaultConstructor() {
+		Deck d = new Deck();
+		assertEquals(new ArrayList<Card>(), d._notPlayed);
+		assertEquals(new ArrayList<Card>(), d._hand);
+		assertEquals(new ArrayList<Card>(), d._discard);
+	}
+	
+	@Test
+	public void testConstructorWNotPlayed() {
+		ArrayList<Card> notPlayed = randomCardList();
+		Deck d = new Deck(notPlayed);
+		assertEquals(notPlayed, d._notPlayed);
+		assertEquals(new ArrayList<Card>(), d._hand);
+		assertEquals(new ArrayList<Card>(), d._discard);
+	}
+	
+	@Test
+	public void testConstructorWAll() {
+		ArrayList<Card> notPlayed = randomCardList();
+		ArrayList<Card> hand = randomCardList();
+		ArrayList<Card> discard = randomCardList();
+		Deck d = new Deck(notPlayed, hand, discard);
+		assertEquals(notPlayed, d._notPlayed);
+		assertEquals(hand, d._hand);
+		assertEquals(discard, d._discard);
+	}
+	
+	@Test
+	public void testDrawCardWEmpty() {
+		Deck d = new Deck();
+		assertFalse(d.DrawCard());
+	}
+	
+	@Test
+	public void testDrawCardWNormal() {
+		ArrayList<Card> notPlayed = randomCardList();
+		ArrayList<Card> notPlayedOrigional = new ArrayList<Card>();
+		notPlayedOrigional.addAll(notPlayed);
+		
+		ArrayList<Card> hand = randomCardList();
+		ArrayList<Card> handOrigional = new ArrayList<Card>();
+		handOrigional.addAll(hand);
+		
+		ArrayList<Card> discard = randomCardList();
+		ArrayList<Card> discardOrigional = new ArrayList<Card>();
+		discardOrigional.addAll(discard);
+		
+		Deck d = new Deck(notPlayed, hand, discard);
+		assertTrue(d.DrawCard());
+		
+		//make sure sizes changed appropriately
+		assertEquals(notPlayedOrigional.size() - 1, notPlayed.size());
+		assertEquals(handOrigional.size() + 1, hand.size());
+		assertEquals(discardOrigional.size(), discard.size());
+		
+		//use set logic to make sure all sets are correct
+		assertFalse(notPlayed.containsAll(notPlayedOrigional));
+		assertTrue(notPlayedOrigional.containsAll(notPlayed));
+		
+		assertFalse(handOrigional.containsAll(hand));
+		assertTrue(hand.containsAll(handOrigional));
+		
+		assertTrue(discard.containsAll(discardOrigional));
+		assertTrue(discardOrigional.containsAll(discard));
+	}
+	
+	private static ArrayList<Card> randomCardList() {
+		ArrayList<Card> ret = new ArrayList<Card>();
+		Random generator = new Random();
+		int c = generator.nextInt(10) + 1;
+		for(int i = 0; i < c; i++) {
+			ret.add(new Card());
+		}
+		return ret;
 	}
 
 }
