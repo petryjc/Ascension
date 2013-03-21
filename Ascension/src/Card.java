fe2859ce6310e8899e5ef69fcaca686b18da6180
faulknks <faulknks@rose-hulman.edu>
2013-03-20 22:38:26 -0400

    card stuff

--- Ascension/src/Card.java
+++ Ascension/src/Card.java
@@ -4,8 +4,42 @@ import java.awt.Rectangle;
 
 public class Card {
 	
-	private Rectangle location;
+	public enum Faction {
+		Mechana, Lifebound, Void, Enlightened
+	}
 	
+	public enum Type {
+		Hero, Construct, Monster
+	}
+	
+	private Rectangle location;
+	private Type type;
+	private Faction faction;
+	
+	public Rectangle getLocation() {
+		return this.location;
+	}
+	
+	public void setLocation(Rectangle r) {
+		this.location = r;
+	}
+	
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public Faction getFaction() {
+		return faction;
+	}
+
+	public void setFaction(Faction faction) {
+		this.faction = faction;
+	}
+
 	public boolean onCard(Point p) {
 		return location.contains(p);
 	}
--- Ascension/src/CardTest.java
+++ Ascension/src/CardTest.java
@@ -1,7 +1,9 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.awt.Point;
+import java.awt.Rectangle;
 
+import org.junit.Test;
 
 public class CardTest {
 
@@ -10,5 +12,51 @@ public class CardTest {
 		Card c= new Card();
 		assertNotNull(c);
 	}
-
+	
+	@Test
+	public void testType() {
+		Card c = new Card();
+		c.setType(Card.Type.Construct);
+		assertEquals(c.getType(), Card.Type.Construct);
+		c.setType(Card.Type.Hero);
+		assertEquals(c.getType(), Card.Type.Hero);
+		c.setType(Card.Type.Monster);
+		assertEquals(c.getType(), Card.Type.Monster);
+	}
+	
+	@Test
+	public void testFaction() {
+		Card c = new Card();
+		c.setFaction(Card.Faction.Enlightened);
+		assertEquals(c.getFaction(), Card.Faction.Enlightened);
+		c.setFaction(Card.Faction.Void);
+		assertEquals(c.getFaction(), Card.Faction.Void);
+		c.setFaction(Card.Faction.Lifebound);
+		assertEquals(c.getFaction(), Card.Faction.Lifebound);
+		c.setFaction(Card.Faction.Mechana);
+		assertEquals(c.getFaction(), Card.Faction.Mechana);
+	}
+	
+	@Test
+	public void testLocation() {
+		Rectangle r = new Rectangle(10, 10, 50, 50);
+		Card c = new Card();
+		c.setLocation(r);
+		assertEquals(new Rectangle(10, 10, 50, 50), c.getLocation());
+		c.setLocation(new Rectangle(15, 12, 30, 70));
+		assertEquals(new Rectangle(15, 12, 30, 70), c.getLocation());
+		c.setLocation(new Rectangle(1, 2, 3, 4));
+		assertEquals(new Rectangle(1, 2, 3, 4), c.getLocation());
+	}
+	
+	@Test
+	public void testOnCard() {
+		Card c = new Card();
+		Rectangle r = new Rectangle(10, 10, 50, 50);
+		Point p = new Point(30, 30);
+		c.setLocation(r);
+		assertTrue(c.onCard(p));
+		p = new Point(120, 120);
+		assertFalse(c.onCard(p));
+	}
 }
